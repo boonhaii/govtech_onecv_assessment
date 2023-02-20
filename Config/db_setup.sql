dabee33f29e5d55dc2b4718fc0dbed1238396933
boonhaii <boonzxc@gmail.com>
2023-02-20 23:46:47 +0800

    Update sql commands

--- Config/db_setup.sql
+++ Config/db_setup.sql
@@ -1,16 +1,26 @@
 -- Already added locally, but present just for recording keeping purposes.
 
+-- Creates the Database
+-- Replace with api_db if need to create test database
+CREATE DATABASE apidb;
+
+-- Changes to the desired database
+USE apidb;
+
+-- Creates the Table Teachers
 CREATE TABLE Teachers( 
   email VARCHAR(320), 
   PRIMARY KEY(email)
 );
 
+-- Creates the Table Students
 CREATE TABLE Students(
   email VARCHAR(320), 
   suspended TINYINT, 
   PRIMARY KEY(email)
 );
 
+-- Creates the Table Registers
 CREATE TABLE Registers( 
   t_email VARCHAR(320), 
   s_email VARCHAR(320), 
